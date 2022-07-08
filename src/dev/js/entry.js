e683463be7ff66f5fc8d683cc6cb168aee6e04f5
Saurabh Datar <sdatar2@stanford.edu>
2022-07-08 08:41:39 -0400

    first commit

---
+++ src/dev/js/entry.js
@@ -0,0 +1,7 @@
+window.onResize = (width) => {
+  console.log(width);
+};
+
+window.enterView = (msg) => {
+  console.log('enter-view', msg);
+};
---
+++ src/main.js
@@ -0,0 +1,28 @@
+'use strict';
+(function() {
+  // global variables
+
+
+
+  // called once on page load
+  var init = function() {
+
+
+  };
+
+  // called automatically on article page resize
+  window.onResize = function(width) {
+
+  };
+
+  // called when the graphic enters the viewport
+  window.enterView = function() {
+
+  };
+
+
+  // graphic code
+
+  // run code
+  init();
+})();
